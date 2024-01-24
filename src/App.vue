3ae82e0ab9e973cc74aac7a7cc263b4aac921d9b
reinhardt5160 <reinhardt.swart@students.iaac.net>
2024-01-23 19:19:51 -0700

    added base template and style

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,23 @@ import { ref } from "vue" // you need this to use ref()
 <!-- the template is where the html code goes -->
 <template>
 
+    <div id="navbar" class="container">  
+            
+        <div id="title">Reinhardt's newest website</div>
+            <!-- <div id="logo">
+                <img src="cow.jpg" alt="macad cow"> -->
 
+            <!-- </div> -->
+            
+        </div>
+
+        <div id="flex">
+
+            <div id="sidebar" class="container"> Sidebar </div>
+
+            <div id="main" class="container"> What the cow said </div>
+
+        </div>
 
 </template>
 
@@ -19,5 +35,70 @@ import { ref } from "vue" // you need this to use ref()
 <!-- style is where the css code goes -->
 <style scoped>
 
+    body{
+
+        margin:0;
+    }
+
+    div{
+
+        /* put the borders in the inside of container */
+        box-sizing: border-box;
+    }
+
+    img{
+
+        height: 100%;
+        width: auto;
+
+    }
+
+    #navbar{
+
+        height: 50px;
+        border-color: red;
+        text-align: center;
+        vertical-align:baseline;
+        font-size: 25px;
+    }
+
+    #flex{
+        
+        display: flex;
+        height: calc(100vh - 50px);
+    }
+
+    #sidebar{
+        
+        width:30%;
+        border-color: blue;
+
+    }
+
+    #main{
+        width:70%;
+        border-color: green;
+
+    }
+
+    #title{
+        width: 70%;
+        float:left;
+    }
+
+    #logo{
+        width: 30%;
+        height: 100%;
+        float:right;
+        text-align: right;
+
+    }
+
+    .container{
+
+        border-style: dotted;
+        border-width: 1px;
+
+    }
 
 </style>
